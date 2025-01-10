97b99a6dcdcc776f562a6256673780a26c5cb34d
terphix <terphix.code@yahoo.com>
2025-01-11 01:23:50 +0700

    Helix Cachix

--- users/terphix/nixos/system/nix.nix
+++ users/terphix/nixos/system/nix.nix
@@ -11,10 +11,14 @@
         "flakes"
       ];
 
-      substituters = [ "https://hyprland.cachix.org" ];
+      substituters = [
+        "https://hyprland.cachix.org"
+        "https://helix.cachix.org"
+      ];
 
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
       ];
     };
     gc = {
