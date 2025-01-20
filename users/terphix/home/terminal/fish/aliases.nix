f6603ceb3ff5b9c7f59db302f9a414e4272c807d
terphix <terphix.code@yahoo.com>
2025-01-20 20:26:02 +0700

    Torrent client

--- users/terphix/home/terminal/fish/aliases.nix
+++ users/terphix/home/terminal/fish/aliases.nix
@@ -7,6 +7,7 @@ in
     shell = "nix-shell --command fish ./Shell";
     rb = "sudo nixos-rebuild switch --flake ${configPath}";
     gc = "sudo nix-collect-garbage -d";
+    shut = "shutdown now";
 
     ls = "lsd";
     ff = "fastfetch";
