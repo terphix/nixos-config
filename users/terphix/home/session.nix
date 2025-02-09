b21e6dcc92b81b4b2e63c32ba685e4f3cf3146aa
terphix <terphix.code@yahoo.com>
2025-02-09 11:36:32 +0700

    Fix cursor

--- users/terphix/home/session.nix
+++ users/terphix/home/session.nix
@@ -5,7 +5,6 @@ in
 {
   home.sessionVariables = {
     inherit config;
-    GDK_SCALE = "2";
     GTK_USE_PORTAL = "1";
   };
 }
--- users/terphix/home/wm/theme/cursor.nix
+++ users/terphix/home/wm/theme/cursor.nix
@@ -7,6 +7,8 @@
   };
 
   home.pointerCursor = {
+    x11.enable = true;
+    gtk.enable = true;
     name = "catppuccin-macchiato-dark-cursors";
     package = pkgs.catppuccin-cursors.macchiatoDark;
     size = 28;
