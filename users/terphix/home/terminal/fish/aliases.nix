e239d5126bd999b2f115889209ea71d6ebec1c35
terphix <terphix.code@yahoo.com>
2025-03-26 15:48:01 +0700

    Remove secrets

--- users/terphix/home/terminal/fish/aliases.nix
+++ users/terphix/home/terminal/fish/aliases.nix
@@ -5,8 +5,8 @@ in
 {
   programs.fish.shellAliases = {
     shell = "nix-shell --command fish ./Shell";
-    rb = "sudo nixos-rebuild switch --flake ${config}";
-    rb-diff = "sudo nixos-rebuild build --flake ${config} && nvd diff /run/current-system result && rm result";
+    rebuild = "sudo nixos-rebuild switch --flake ${config}";
+    rebuild-diff = "sudo nixos-rebuild build --flake ${config} && nvd diff /run/current-system result && rm result";
     gc = "sudo nix-collect-garbage -d";
     shut = "shutdown now";
 
--- users/terphix/home/terminal/fish/functions.nix
+++ users/terphix/home/terminal/fish/functions.nix
@@ -1,4 +1,11 @@
 {
+  userConfig,
+  ...
+}:
+let
+  inherit (userConfig.paths) config;
+in
+{
   programs.fish.functions = {
     # Fish standart commands
     __fish_command_not_found_handler = {
@@ -7,5 +14,29 @@
     };
 
     fish_greeting = "";
+
+    rb = ''
+      if test -d ${config}/secrets
+        echo "Error: secrets dir exists!"
+        return 1
+      else if not test -d ${config}/sops-secrets
+        echo "Error: sops-secrets dir doesn't exists"
+        return 1
+      else
+        mkdir ${config}/secrets && \
+        cp -r ${config}/sops-secrets/*.* ${config}/secrets/ && \
+        git add ${config}/secrets/*.*
+        
+        switch $argv
+          case "diff"
+            rebuild-diff
+          case "*"
+            rebuild
+        end
+        
+        git --git-dir ${config}/.git reset ${config}/secrets/*.* && \
+        rm -rf ${config}/secrets
+      end
+    '';
   };
 }
